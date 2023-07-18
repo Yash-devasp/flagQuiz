bb55bacdc6968cfb7b07d9e6412c499af507212d
Yash-devasp <yashgoswami148@gmail.com>
2023-07-18 22:25:36 +0530

    Initial Commit

---
+++ guessFlag/guessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessFlagApp.swift
+//  guessFlag
+//
+//  Created by Yash Goswami on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct guessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
